16ede290c665e84adb822b5a4f288bfbb5e2f77d
srazavi4 <razavisaleh36@gmail.com>
2025-02-14 18:32:58 -0600

    Added functions for customer table

--- BACKEND/operations.py
+++ BACKEND/operations.py
@@ -52,3 +52,47 @@ def delete_book(id):
     connection.commit()
     cursor.close()
     connection.close()
+
+# Customer Functions
+# Viewing customer function
+def get_all_customers():
+    connection = db.get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM customers")
+    customers = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return customers
+
+# Add customer function
+def add_customer(firstname, lastname, email, passwordhash):
+    connection = db.get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(
+        "INSERT INTO customers (firstname, lastname, email, passwordhash) VALUES (%s, %s, %s, %s)",
+        (firstname, lastname, email, passwordhash)
+    )
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+# Update customer function
+def update_customer(id, firstname, lastname, email, passwordhash):
+    connection = db.get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(
+        "UPDATE customers SET firstname = %s, lastname = %s, email = %s, passwordhash = %s WHERE id = %s",
+        (firstname, lastname, email, passwordhash, id)
+    )
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+# Delete customer function
+def delete_customer(id):
+    connection = db.get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM customers WHERE id = %s", (id,))
+    connection.commit()
+    cursor.close()
+    connection.close()
